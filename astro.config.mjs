5f7c1d978d8c5f617a16a6ade983e88177a1f37a
nonl4331 <nicholasl0003@gmail.com>
2026-01-17 20:24:06 +1100

    Setup astro gh action + CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://nonl.knao.net",
   integrations: [mdx({
 	remarkPlugins: [remarkMath],
 	rehypePlugins: [rehypeKatex],
